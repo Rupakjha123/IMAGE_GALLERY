411bbc9f4e6c0ef90ed18d34c7300b71bf6474f0
RUPAK Kumar jha <roopakjha123@gmail.com>
2025-06-10 13:40:18 +0530

    Add files via upload

---
+++ imagegallery.js
@@ -0,0 +1,21 @@
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImage");
+const images = document.querySelectorAll(".gallery img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
